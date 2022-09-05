a9794dc759f708bb9da891fe347ff4088cbae052
Mike Shea <mikesheav@gmail.com>
2022-09-04 22:30:29 -0700

    adds filter list to invoice list

---
+++ components/useHandleClickOutside.tsx
@@ -0,0 +1,25 @@
+import { useState, useEffect, useCallback, useRef } from 'react';
+
+export default function useHandleClickOutside(initialIsVisible: boolean) {
+  const [isComponentVisible, setIsComponentVisible] = useState(initialIsVisible);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = useCallback(
+    (event: MouseEvent) => {
+      if (isComponentVisible && ref.current && !ref.current.contains(event.target as Node)) {
+        setIsComponentVisible(false);
+        console.log('clicked outside');
+      }
+    },
+    [isComponentVisible]
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, [handleClickOutside]);
+
+  return { componentVisibleRef: ref, isComponentVisible, setIsComponentVisible };
+}
---
+++ components/usePortal.tsx
@@ -0,0 +1,24 @@
+import { ReactNode, useCallback, useEffect, useMemo } from 'react';
+import { createPortal } from 'react-dom';
+
+interface usePortalProps {
+  elementId: string;
+  componentToInsert: ReactNode;
+}
+
+export default function usePortal({ elementId, componentToInsert }: usePortalProps) {
+  const newDiv = useMemo(() => {
+    const exisitingDiv = document.querySelector(`#${elementId}`);
+    if (exisitingDiv) return exisitingDiv;
+    const element = document.createElement('div');
+    element.id = elementId;
+    document.body.appendChild(element);
+    return element;
+  }, [elementId]);
+
+  const portal = createPortal(componentToInsert, document.querySelector(`#${elementId}`) ?? newDiv);
+
+  return {
+    portal
+  };
+}
