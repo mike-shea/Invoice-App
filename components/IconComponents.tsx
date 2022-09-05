99033d9a369d150f53095894d684012c0dfe748f
Mike Shea <mikesheav@gmail.com>
2022-09-04 22:35:16 -0700

    Adds Chevron-Left SVG to component list

--- components/IconComponents.tsx
+++ components/IconComponents.tsx
@@ -96,6 +96,24 @@ export function ChevronRightSvg(props: IconSVGProps) {
   );
 }
 
+export function ChevronLeftSvg(props: IconSVGProps) {
+  return (
+    <svg
+      className={props.classGroup}
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        className={props.className}
+        d="M15.705 7.41L14.295 6L8.29501 12L14.295 18L15.705 16.59L11.125 12L15.705 7.41Z"
+        fill="#353535"
+      />
+    </svg>
+  );
+}
+
 export function TrashCanSvg(props: IconSVGProps) {
   return (
     <svg
