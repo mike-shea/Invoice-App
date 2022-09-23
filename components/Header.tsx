783da53bd25f4794eae4ea04563fbec70a9d4e99
Mike Shea <mikesheav@gmail.com>
2022-09-23 14:35:37 -0700

    adds Header.tsx and Nav.tsx back into the mix

---
+++ components/Header.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import FilterListPopUp from './filterListPopUp';
+import { ExpandMoreSvg, PlusSvg } from './IconComponents';
+import { FilteredStatusType } from './types';
+import useHandleClickOutside from '../hooks/useHandleClickOutside';
+
+export default function Header(props: {
+  filterByStatus: FilteredStatusType;
+  setFilterByStatus: React.Dispatch<React.SetStateAction<FilteredStatusType>>;
+  invoiceItemLength: number;
+  mountForm: () => void;
+  clearForm: () => void;
+}) {
+  const { componentVisibleRef, isComponentVisible, setIsComponentVisible } =
+    useHandleClickOutside(false);
+
+  function newInvoice() {
+    props.mountForm();
+  }
+
+  return (
+    <header className="flex w-full flex-row items-center justify-between">
+      <div className="flex flex-col lg:gap-2">
+        <h1 className="text-xl font-bold leading-tight text-slate-800 dark:text-slate-200 lg:text-5xl">
+          Invoices
+        </h1>
+
+        <p className="text-sm text-slate-500 dark:text-slate-500">
+          <span className="hidden lg:inline-block">There are&nbsp;</span>
+          {props.invoiceItemLength ?? 0}
+          <span className="hidden lg:inline-block">&nbsp;total</span>&nbsp;invoices
+        </p>
+      </div>
+
+      <div className="flex flex-wrap items-center gap-3 text-sm md:text-base lg:gap-10">
+        <div className="relative">
+          <button
+            onClick={() => setIsComponentVisible((prev) => !prev)}
+            className={`flex items-center rounded-full px-3 py-2 font-semibold text-slate-700 transition hover:bg-slate-200 dark:font-normal  dark:hover:bg-slate-800/70 ${
+              isComponentVisible
+                ? 'bg-slate-200 dark:bg-slate-800/70 dark:text-slate-300'
+                : 'dark:text-slate-300'
+            }`}>
+            <span>Filter&nbsp;</span>
+            <span className="hidden lg:inline-block"> by status </span>
+            <span className="pl-1 lg:pl-2">
+              <ExpandMoreSvg className="fill-blue-600" />
+            </span>
+          </button>
+
+          {isComponentVisible && (
+            <FilterListPopUp
+              componentVisibleRef={componentVisibleRef}
+              filterByStatus={props.filterByStatus}
+              setFilterByStatus={props.setFilterByStatus}
+            />
+          )}
+        </div>
+
+        <button
+          onClick={newInvoice}
+          className="flex items-center gap-2 rounded-full bg-blue-600 py-2 pl-2 pr-5 font-semibold text-white transition hover:bg-blue-700 active:bg-blue-800 dark:bg-blue-800 dark:hover:bg-blue-900 lg:gap-4">
+          <div className="rounded-full bg-white p-1">
+            <PlusSvg className="fill-blue-500" />
+          </div>
+          <span>
+            New&nbsp;<span className="hidden lg:inline-block">Invoice</span>
+          </span>
+        </button>
+      </div>
+    </header>
+  );
+}
---
+++ components/Nav.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+import { LogoSvg2, MoonSvg, SunSvg } from './IconComponents';
+import Avatar from '../public/crow-1.svg';
+
+function Nav() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <>
+      <div className="hidden min-h-screen w-28 flex-col bg-slate-100 transition dark:bg-slate-900 lg:flex"></div>
+      <nav className="z-30 flex w-full justify-between bg-slate-800 pr-8 lg:fixed lg:min-h-screen lg:w-24 lg:flex-col lg:rounded-r-3xl lg:pr-0 lg:pb-8">
+        <div className="relative w-24 overflow-hidden rounded-r-3xl bg-blue-500 dark:bg-blue-700 lg:rounded-br-3xl">
+          <div className="absolute bottom-0 h-1/2 w-full rounded-tl-3xl bg-white/10"></div>
+          <div id="logo-container" className="p-5">
+            <LogoSvg2 classGroup="w-full h-full" className="fill-white" />
+          </div>
+        </div>
+
+        <div className="flex items-center gap-8 lg:flex-col">
+          <button
+            onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            className="group aspect-square w-12 overflow-hidden rounded-full bg-slate-900/50 p-2">
+            {theme === 'light' ? (
+              <SunSvg
+                classGroup="w-full h-full"
+                className="fill-slate-500 transition group-hover:fill-slate-200"
+              />
+            ) : (
+              <MoonSvg
+                classGroup="w-full h-full"
+                className="fill-slate-500 transition group-hover:fill-slate-200"
+              />
+            )}
+          </button>
+          <div className="aspect-square w-12 overflow-hidden rounded-full bg-blue-500 p-2">
+            <Image priority={true} layout="responsive" alt="logo" src={Avatar} />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Nav;
